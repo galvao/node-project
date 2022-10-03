3bcdf60f7f45570c0df0ef44714c970ae81f7897
Er Galvão Abbott <galvao@galvao.eti.br>
2022-10-03 04:05:30 -0300

    Working with the EventEmitter(comes with Node); Extending the emitter via inheritance; Events are synchronous.

--- app.js
+++ app.js
@@ -1,4 +1,33 @@
-const tutorial = require('./tutorial');
-console.log(tutorial.sum(1, 1));
-console.log(tutorial.PI);
-console.log(new tutorial.SomeMathObject());
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('tutorial', (num1, num2) => {
+    console.log(num1 + num2);
+});
+
+eventEmitter.emit('tutorial', 1, 2);
+
+class Person extends EventEmitter {
+    constructor(name) {
+        super();
+        this._name = name;
+    }
+
+    get name() {
+        return this._name;
+    }
+}
+
+let galvao = new Person('Galvão');
+
+galvao.on('name', () => {
+    console.log('my name is ' + galvao.name);
+});
+
+let christina = new Person('Christina');
+christina.on('name', () => {
+    console.log('my name is ' + christina.name);
+});
+
+galvao.emit('name');
+christina.emit('name');
